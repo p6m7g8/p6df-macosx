dc7c281a124bfaee12bb4951b291a6c17a522c26
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-19 23:14:17 -0400

    Initial commit

---
+++ init.zsh
@@ -0,0 +1,11 @@
+p6df::modules::macosx::version() { echo "0.0.1" }
+p6df::modules::macosx::deps()    { 
+	ModuleDeps=()
+}
+
+p6df::modules::macosx::external() { 
+}
+
+p6df::modules::macosx::init() {
+}
+
