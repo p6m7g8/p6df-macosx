cc61fcad876c08c803be1ceecb1c1a4d752f58eb
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-04-03 21:27:16 -0400

    sort

--- init.zsh
+++ init.zsh
@@ -5,43 +5,55 @@ p6df::modules::macosx::deps()    {
 
 p6df::modules::macosx::external::brew() {
 
-   brew tap caskroom/cask
-   brew tap caskroom/versions
+  brew tap caskroom/cask
+  brew tap caskroom/versions
 
-   brew cask install xquartz
-   brew install freerdp
+  ## Remote Desktop Compat
+  brew cask install xquartz
+  brew install freerdp
 
-   brew cask install alfred
-#   brew cask install amazon-drive
-   brew cask install amazon-workspaces
-   brew cask install amazon-chime
-   brew cask install bartender
-   brew cask install dash
-   brew cask install dropbox
-   brew cask install evernote
-   brew cask install firefox
-   brew cask install gitx
-   brew cask install google-chrome
-   # Although google-backup-and-sync may be installed alongside google-drive-file-stream, you should not use the same account with both.
-   # https://support.google.com/a/answer/7496409#allowboth
-   # brew cask install google-drive-file-stream
-   brew cask install google-backup-and-sync
+  ## Amazon
+  brew cask install amazon-workspaces
+  brew cask install amazon-chime
+  brew cask install amazon-drive
 
-   brew cask install google-hangouts
-   brew cask install iterm2
-   brew cask install onedrive
-   brew cask install openoffice
-   brew cask install omnigraffle
-   brew cask install slack
-   brew cask install skitch
-   brew cask install skype
-   brew cask install squidman
-   brew cask install vagrant
-   brew cask install virtualbox
-   brew cask install vlc
-   brew cask install vmware-fusion
-   brew cask install wireshark
-   brew cask install zoomus
+  ## Google
+  brew cask install google-chrome
+  brew cask install google-hangouts
+  # https://support.google.com/a/answer/7496409#allowboth: brew cask install google-drive-file-stream
+  brew cask install google-backup-and-sync
+  
+  ## Microsoft
+  brew cask install microsoft-edge
+  brew cask install microsoft-excel
+  brew cask install microsoft-lync
+  brew cask install microsoft-office
+  brew cask install onedrive
+  brew cask install evernote
+  brew cask install skype
+  brew cask install skype-for-business
+
+  ## Mac
+  brew cask install alfred
+  brew cask install dash
+  brew cask install iterm2
+  brew cask install skitch
+
+  ## Other 
+  brew cask install bartender
+  brew cask install dropbox  
+  brew cask install firefox
+  brew cask install gitx
+  # brew cask install openoffice
+  # brew cask install omnigraffle
+  brew cask install slack
+  brew cask install squidman
+  brew cask install vagrant
+  brew cask install virtualbox
+  brew cask install vlc
+  brew cask install vmware-fusion
+  brew cask install wireshark
+  brew cask install zoomus
 }
 
 p6df::modules::macosx::init() {
