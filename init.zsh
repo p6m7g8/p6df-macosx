008033bcd10f7ce797cd47656ec76bfb066dcffe
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-01-25 16:31:19 -0500

    feat(brew): replaces microsoft-office with individual casks

--- init.zsh
+++ init.zsh
@@ -40,11 +40,16 @@ p6df::modules::macosx::external::brew() {
 #  brew install --cask google-backup-and-sync
   
   ## Microsoft
+  brew install --cask onedrive
   brew install --cask microsoft-edge
-#  brew install --cask microsoft-excel
+  brew install --cask microsoft-outlook
+  brew install --cask microsoft-excel
+  brew install --cask microsoft-word
+  brew install --cask microsoft-powerpoint
+  # XXX: no cask for onenote, only in microsoft-office
+  
+  ## Old Microsoft
   brew install --cask microsoft-lync
-  brew install --cask microsoft-office
-#  brew install --cask onedrive
   brew install --cask evernote
   brew install --cask skype
   brew install --cask skype-for-business
